99ac15fb252f4f0cf9f6fce76426ba2311c0352e
Peter Tascio <peter.tascio@gmail.com>
2017-06-27 14:47:21 -0400

    Add story instructions routes

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,8 @@ Rails.application.routes.draw do
   resources :wordforms
   resources :mailers, only: [:create]
 
-  get ':instructions' => 'static#instructions'
+  get '/instructions' => 'statics#instructions'
+  get '/storyinstructions' => 'statics#storyinstructions'
   get 'auth/facebook/callback', to: 'sessions#create'
   get 'auth/facebook/callback', to: 'users#create'
   get 'auth/failure', to: redirect("/")
