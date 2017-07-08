df349dc988cb118c8337346785e2e720a4000764
Peter Tascio <peter.tascio@gmail.com>
2017-07-08 15:22:17 -0400

    Add JS

---
+++ app/assets/javascripts/main.js
@@ -0,0 +1,20 @@
+var buttons;
+ var searcharea;
+ var showing = false;
+ function toggleSearchBar(){
+   if (!showing){
+     searcharea.style.display = "block";
+     showing = true;
+   }else {
+     searcharea.style.display = "none";
+     showing = false;
+   }
+ }
+
+document.addEventListener("DOMContentLoaded", () => {
+  searcharea = document.getElementById("searcharea");
+  buttons = document.getElementsByClassName("glyphicon");
+  for (var i = 0; i < buttons.length; i++){
+    buttons[i].addEventListener("click", toggleSearchBar);
+  }
+ });
