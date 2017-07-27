d74310e9bcd91c2e800c0cd787c6ec0dab44d5a5
Peter Tascio <peter.tascio@gmail.com>
2017-07-27 11:13:28 -0400

    Redo create in sessions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,14 +1,14 @@
 class SessionsController < ApplicationController
 
   def create
-    if (params[:user][:username].length < 1 || params[:user][:password].length < 1)
-      redirect_to session_path, :flash => { :error => "Make sure both Username and Password are filled in." }
-      return
-    end
+
     if request.env['omniauth.auth']
       user = User.create_with_omniauth(request.env['omniauth.auth'])
       login!(user)
       redirect_to root_path
+    elsif (params[:user][:username].length < 1 || params[:user][:password].length < 1)
+      redirect_to session_path, :flash => { :error => "Make sure both Username and Password are filled in." }
+      return
     else
       user = User.find_by_credentials(
         params[:user][:username],
