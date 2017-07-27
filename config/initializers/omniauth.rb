2e20e3ddfcfc443efe3ab65cd449756bd71c58fb
Peter Tascio <peter.tascio@gmail.com>
2017-07-27 11:08:39 -0400

    Add comma

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,6 +1,6 @@
 OmniAuth.config.logger = Rails.logger
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, ENV['FACEBOOK_APP_ID'], ENV['FACEBOOK_APP_SECRET']
+  provider :facebook, ENV['FACEBOOK_APP_ID'], ENV['FACEBOOK_APP_SECRET'],
   scope: 'public_profile', info_fields: 'id, name, link',
   client_options: {:ssl => {:ca_file => '/usr/lib/ssl/certs/ca-certificates.crt'}}
 end
