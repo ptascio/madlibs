0b4c133eb57c098d7f9b1fe4917168c80e69133d
Peter Tascio <peter.tascio@gmail.com>
2017-07-27 11:06:09 -0400

    Change omniauth config

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,7 +1,6 @@
 OmniAuth.config.logger = Rails.logger
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, Rails.application.secrets.facebook_app_id,
-  Rails.application.secrets.facebook_app_secret, scope:
-  'public_profile', info_fields: 'id, name, link',
+  provider :facebook, ENV['FACEBOOK_APP_ID'], ENV['FACEBOOK_APP_SECRET']
+  scope: 'public_profile', info_fields: 'id, name, link',
   client_options: {:ssl => {:ca_file => '/usr/lib/ssl/certs/ca-certificates.crt'}}
 end
