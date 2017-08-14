9f6a9cc2bbd7d776677152606d347126456692bf
Peter Tascio <peter.tascio@gmail.com>
2017-08-14 11:23:06 -0400

    Reconfig email

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -57,10 +57,11 @@ config.action_mailer.default_url_options = { host: 'localhost', port: 3000}
 config.action_mailer.smtp_settings = {
     address: "smtp.gmail.com",
     port: 587,
-    domain: "gmail.com",
-    authentication: "plain",
+    domain: "mail.google.com",
     user_name: ENV['GMAIL_USERNAME'],
-    password: ENV["GMAIL_PASSWORD"]
+    password: ENV["GMAIL_PASSWORD"],
+    authentication: "plain",
+    enable_starttls_auto: true
   }
 
   # Raises error for missing translations
