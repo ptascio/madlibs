dc2cc46598e2d098d2b970b8bfeb22c49b875109
Peter Tascio <peter.tascio@gmail.com>
2017-08-14 11:29:35 -0400

    Try again

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,7 +83,7 @@ Rails.application.configure do
 # }
 config.action_mailer.perform_deliveries = true
 config.action_mailer.raise_delivery_errors = true
-config.action_mailer.default_url_options = { host: 'madlibsonrails.herokuapp.com'}
+
 config.action_mailer.smtp_settings = {
     address: "smtp.gmail.com",
     port: 587,
