0c35cf3ce710895252028d9d5a6667efe6ec1229
Peter Tascio <peter.tascio@gmail.com>
2017-08-14 11:44:31 -0400

    Change email subject

--- app/mailers/mailer.rb
+++ app/mailers/mailer.rb
@@ -3,7 +3,7 @@ class Mailer < ApplicationMailer
 
   def sample_email(email)
     mail(to: email,
-    subject: "Thank you for signing up",
+    subject: "Thank you for emailing MadlibsOnRails",
     template_path: 'mailer',
     template_name: 'sample_email')
   end
