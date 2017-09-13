8ef3c7c9bde064adb44f5c77f7688b5a8d51402c
Peter Tascio <peter.tascio@gmail.com>
2017-09-13 12:27:48 -0400

    Finish index tests for books controller

--- spec/controllers/book_controllers_spec.rb
+++ spec/controllers/book_controllers_spec.rb
@@ -13,7 +13,7 @@ RSpec.describe BooksController, :type => :controller do
     let(:user) { User.create!(username: "Todd", password: 'password') }
     before { allow(controller).to receive(:current_user) { user } }
     before do
-          @todd_book = Book.create!(title: "Todd's Book", exclusive: 1, user_id: "#{user.id}")
+      @todd_book = Book.create!(title: "Todd's Book", exclusive: 1, user_id: "#{user.id}")
     end
     it "finds all standard books" do
       books = Book.where(:user_id => nil, :exclusive => 0)
@@ -36,19 +36,31 @@ RSpec.describe BooksController, :type => :controller do
   end
 
   describe "GET index if current_user.nil?" do
+    before do
+      @user = nil
+    end
     it "finds all standard books" do
       books = Book.where(:user_id => nil, :exclusive => 0)
       expect(books.length).to eq(2)
     end
 
     it "finds does not find exclusive books" do
+      if @user
         ex_books = Book.where(:user_id => nil, :exclusive => 1)
-        expect(ex_books.length).to eq(nil)
+      end
+        expect(ex_books).to eq(nil)
     end
 
     it "finds shared books" do
         shared_books = Book.where(:user_id => !nil, :exclusive => 0)
         expect(shared_books.length).to eq(1)
+    end
+
+    it "does not find users books if user.nil?" do
+      unless !@user
+        books = Book.where(:user_id => "#{@user.id}")
+      end
+      expect(books).to eq(nil)
     end
   end
 end
