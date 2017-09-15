158a31994dead96bb99a208f23aed86a61b6b215
Peter Tascio <peter.tascio@gmail.com>
2017-09-15 11:41:10 -0400

    Index tests for madlibs controller

--- spec/controllers/madlibs_controller_spec.rb
+++ spec/controllers/madlibs_controller_spec.rb
@@ -1,11 +1,25 @@
 require "rails_helper"
 
 RSpec.describe MadlibsController, :type => :controller do
+  before do
+    Book.create!(title: "The First Book")
+  end
+  describe "GET #index" do
+    before do
+      Madlib.create!(title: "I like books", story: "I like _noun_", book_id: 1)
+    end
+    it "fetches all Madlibs associated with book_id" do
+      get :index, params: {book_id: 1}
+      expect(assigns(:madlibs).count).to be(1)
+    end
+    it "renders index" do
+      get :index, params: {book_id: 1}
+      expect(response).to render_template('index')
+    end
+  end
 
   describe "POST #create" do
-    before do
-      Book.create!(title: "The First Book")
-    end
+
     it "requires book_id params" do
       post :create, params: {:book_id => 1, :madlib => {title: "Num 1", story: "I like _noun_", book_id: 1}}
       expect(Madlib.count).to be(1)
@@ -22,7 +36,7 @@ RSpec.describe MadlibsController, :type => :controller do
     end
 
     it "creates a wordform for the story" do
-      post :create, params: {:book_id => 1, :madlib => {title: "Cake!", story: "I like _cake_", book_id: 1}}
+      post :create, params: {:book_id => 1, :madlib => {title: "Cake!", story: "I like _noun_", book_id: 1}}
       expect(Wordform.first.madlib_id).to be(Madlib.first.id)
     end
   end
